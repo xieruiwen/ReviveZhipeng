05f318e9d1b995d0f8f8c3ff3bf3cefc68ce74df
ruiwen <ruiwen.xie@garena.com>
2023-09-12 13:58:26 +0800

    modified:   HTTPServer.py

--- HTTPServer.py
+++ HTTPServer.py
@@ -4,10 +4,29 @@ import hashlib
 import json
 from typing import Dict, Any
 
+from flask import Flask, request
+
 app = Flask(__name__)
 
 #BOT_WEBHOOK_URL = "https://openapi.seatalk.io/webhook/group/2GR09OlbSzm0mBqt4GPfOQ"
 BOT_WEBHOOK_URL = "https://openapi.seatalk.io/webhook/group/w-a0CCzIQYyGo7anZqd6HA"
+
+
+# settings
+SIGNING_SECRET = b"82KXQ97A01gCjvYJ84O3p8f0lNovjL9Q"
+
+
+# event list
+# ref: https://open.seatalk.io/docs/list-of-events
+EVENT_VERIFICATION = "event_verification"
+NEW_BOT_SUBSCRIBER = "new_bot_subscriber"
+MESSAGE_FROM_BOT_SUBSCRIBER = "message_from_bot_subscriber"
+INTERACTIVE_MESSAGE_CLICK = "interactive_message_click"
+BOT_ADDED_TO_GROUP_CHAT = "bot_added_to_group_chat"
+BOT_REMOVED_FROM_GROUP_CHAT = "bot_removed_from_group_chat"
+NEW_MENTIONED_MESSAGE_RECEIVED_FROM_GROUP_CHAT = "new_mentioned_message_received_from_group_chat"
+
+
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
@@ -29,6 +48,40 @@ def send_webhook():
         return jsonify({"message": "Webhook sent successfully!"}), 200
     else:
         return jsonify({"message": "Failed to send webhook."}), 400
+
+
+def is_valid_signature(signing_secret: bytes, body: bytes, signature: str) -> bool:
+    # ref: https://open.seatalk.io/docs/server-apis-event-callback
+    return hashlib.sha256(body + signing_secret).hexdigest() == signature
+
+
+@app.route("/bot-callback", methods=["POST",'GET'])
+def bot_callback_handler():
+    body: bytes = request.get_data()
+    signature: str = request.headers.get("signature")
+    if not is_valid_signature(SIGNING_SECRET, body, signature):
+        return ""
+    data: Dict[str, Any] = json.loads(body)
+    event_type: str = data.get("event_type", "")
+    if event_type == EVENT_VERIFICATION:
+        return data.get("event")
+    elif event_type == NEW_MENTIONED_MESSAGE_RECEIVED_FROM_GROUP_CHAT:
+        json_payload = {
+                "tag": "text",
+                "text": {
+                    "content": u"咖",
+                    "at_all": False
+                }
+            }
+        response = httpx.post(url=BOT_WEBHOOK_URL, json=json_payload)
+        pass
+    else:
+        pass
+        return ""
+    
+
+
+
 
 
 if __name__ == "__main__":
